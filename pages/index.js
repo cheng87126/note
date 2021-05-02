31fbd4a5fecb4250831c70c0760c8f3a22882429
cheng87126 <cheng87126@gmail.com>
2021-05-02 19:32:21 +0800

    api

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
+import fetch from 'node-fetch'
+
 function HomePage() {
   return <div>Welcome to Next.js!</div>
+}
+
+export async function getServerSideProps() {
+  const res = await fetch('http://localhost:3000/api/list')
+  console.log('res',res)
+  const data = await res.json()
+  return { props: { data } }
 }
 
 export default HomePage
