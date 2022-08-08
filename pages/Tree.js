6d8a4c6edce915f009425be0801168b681fc55bd
cheng87126 <cheng87126@gmail.com>
2022-08-08 22:32:57 +0800

    findPath

--- pages/Tree.js
+++ pages/Tree.js
@@ -1,5 +1,31 @@
-function Tree(){
+import { useEffect } from 'react'
+import tree from '../public/tree.json'
 
+function Tree(){
+    useEffect(()=>{
+        function handle(findId,root){
+            const path = []
+            function findPath(findId,root){
+                for(let i=0;i<root.length;i++){
+                    if(path.length) break
+                    const { id, children } = root[i]
+                    if(findId === id){
+                        return i
+                    }else if(children.length){
+                        const res = findPath(findId,children)
+                        if(res!==void 0) path.unshift(res)
+                    }
+                    if(path.length) {
+                        path.unshift(i)
+                    }
+                }
+            }
+            const res = findPath(findId,root)
+            if(res!==void 0) path.unshift(res)
+            return path
+        }
+        console.log(handle(100,tree),tree)
+    },[])
     return <div>Tree</div>
 }
 
